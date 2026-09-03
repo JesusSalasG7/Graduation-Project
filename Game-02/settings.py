91d7ebe5ad3d1de8e59a3e5bd350dd10a9e36ceb
JesusSalasG7 <jesusalas007@gmail.com>
2026-09-03 17:35:09 -0400

    Agregando juego 6

---
+++ Game-02/settings.py
@@ -0,0 +1,13 @@
+"""
+settings.py
+
+Configuración leída automáticamente por gale.conf.settings: basta con que
+este archivo exista junto a main.py para que Game la resuelva sola.
+"""
+
+TITLE = "2048"
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 850
+VIRTUAL_WIDTH = 700
+VIRTUAL_HEIGHT = 850
+FPS = 60
